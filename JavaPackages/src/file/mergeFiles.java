c624c589a68c9be04a451e1c4ef69e957a8e68ad
Shasanka-shaw1 <shasankashaw5@gmail.com>
2025-09-25 01:47:55 +0530

    i;oh;o

--- JavaPackages/src/file/mergeFiles.java
+++ JavaPackages/src/file/mergeFiles.java
@@ -7,13 +7,14 @@ public class mergeFiles {
         File file=new File("src/file/demoFiles");
 
         PrintWriter printWriter=new PrintWriter("output.txt");
+        //printwriter object makes writing to file conevinient,converts every chracter to text form automatically so you ont need to do typecasting separately
 
-        String[] s=file.list();
-        for(String s1 : s){
+        String[] s=file.list(); //taking the list of all the files in the demoFiles folder
+        for(String s1 : s){ //iterating over every file to copy the contents
             BufferedReader buffer=new BufferedReader(new FileReader("src/file/demoFiles/"+s1));
             String line= buffer.readLine();
             while (line!=null){
-                printWriter.print(line);
+                printWriter.print(line);//the print method directly writes to output.txt
                 line=buffer.readLine();
             }
         }
